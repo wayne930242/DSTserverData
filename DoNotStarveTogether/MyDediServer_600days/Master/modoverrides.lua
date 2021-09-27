9d594f36ae8e7880a4709a7c24c16894446fb9ea
Wei Hung <weihung@vultr.guest>
2021-09-27 21:08:37 +0800

    New Server

---
+++ DoNotStarveTogether/MyDediServer_600days/Master/modoverrides.lua
@@ -0,0 +1,8 @@
+return {
+    --#Simple Health Bar DST
+    ["workshop-1207269058"] = { enabled = true },
+    --#Beefalo Status Bar
+    ["workshop-2477889104"] = { enabled = true },
+    --#Show Bundle
+    ["workshop-1111658995"] = { enabled = true }
+}
